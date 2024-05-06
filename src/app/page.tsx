6d5574da0119aaeb3f1720a139c02d298be20889
sklee-rbrain <sklee-brain@rbrain.co.kr>
2024-05-06 23:57:03 +0900

    tailwind css start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Navigation />
-      <h1>Home@</h1>
+      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
     </div>
   );
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,22 +2,25 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Navigation() {
   const path = usePathname();
 
   return (
     <nav>
-      <ul>
-        <li>
-          <Link href="/">Home </Link>
-          {path === "/" && "🟡"}
-        </li>
-        <li>
-          <Link href="/about-us">About Us </Link>
-          {path === "/about-us" && "🟡"}
-        </li>
-      </ul>
+      <div>
+        <ul>
+          <li>
+            <Link href="/">Home </Link>
+            {path === "/" && "🟡"}
+          </li>
+          <li>
+            <Link href="/about-us">About Us </Link>
+            {path === "/about-us" && "🟡"}
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
