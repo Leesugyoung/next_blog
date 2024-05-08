4227d0bfce28c3604d2b4d7f4e80d7d4ee05665a
Leesugyoung <tnrud4685@naver.com>
2024-05-08 23:05:56 +0900

    - Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from "@/components/Navigation";
-
 export default function Home() {
   return (
     <div>
-      <Navigation />
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
     </div>
   );
