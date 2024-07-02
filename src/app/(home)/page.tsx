8aae8a1ed39bf1a6c22a9335771a770e0df4f34d
Leesugyoung <tnrud4685@naver.com>
2024-07-02 23:14:04 +0900

    + server fecth, loading page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,10 +2,19 @@ export const metadata = {
   title: "Home",
 };
 
-export default function Home() {
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  await new Promise(res => setTimeout(res, 10000));
+  // Next.js는 기본 웹 fetch() API를 확장하여 서버의 각 요청이 자체 영구 캐싱 의미를 설정할 수 있도록 한다.
+  return await fetch(URL).then(res => res.json());
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      <h1 className="text-3xl font-bold underline">{JSON.stringify(movies)}</h1>
     </div>
   );
 }
